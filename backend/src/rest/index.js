4872cf806851a95edc3381e5cd0c82001f777a62
CROSS\Cross <serban_dan13@yahoo.com>
2018-01-09 18:50:49 +0200

    testul lui Dan

--- backend/src/rest/index.js
+++ backend/src/rest/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => res.status(200).send({
-  message: 'Welcome to the API! Test',
+  message: 'Welcome to the API! Testm',
 }));
 
 module.exports = router;
